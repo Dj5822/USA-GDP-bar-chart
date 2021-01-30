14d88dbc38e77ee4ff70d631a4ce14a6a8d9a836
dj5822 <dj5822@gmail.com>
2021-01-30 23:03:30 +1300

    Fixed y axis

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const xPadding = 30;
-const yPadding = 60;
+const xPadding = 60;
+const yPadding = 30;
 
 const width = 1860;
 const height = 820;
@@ -12,12 +12,12 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         // bar horizontal distribution scale.
         const xScale = d3.scaleLinear()
                         .domain([0, dataset.length])
-                        .range([yPadding, width]);
+                        .range([xPadding, width]);
 
         // bar height scale.
         const yScale = d3.scaleLinear()
                         .domain([0, d3.max(dataset, d => d[1])])
-                        .range([0, height-xPadding]);
+                        .range([height-yPadding, 0]);
 
         const xAxis = d3.axisBottom(xScale);
         const yAxis = d3.axisLeft(yScale);
@@ -29,21 +29,21 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         svg.selectAll('rect').data(dataset).enter().append('rect')
             .attr("width", 5)
             .attr("height", (d, i) => {
-                return yScale(d[1]);
+                return height-yPadding-yScale(d[1]);
             })
             .attr("x", (d, i) => {
                 return xScale(i);
             })
             .attr("y", (d, i) => {
-                return height - yScale(d[1]) - xPadding;
+                return yScale(d[1]);
             }).style("fill", "blue");
         
         svg.append('g').attr("id", "x-axis")
-                        .attr("transform", "translate(0," + (height - xPadding) + ")")
+                        .attr("transform", "translate(0," + (height - yPadding) + ")")
                         .call(xAxis);
         
         svg.append('g').attr("id", "y-axis")
-                        .attr("transform", "translate(" + yPadding + ", 0)")
+                        .attr("transform", "translate(" + xPadding + ", 0)")
                         .call(yAxis);
 
         d3.select('body').append('h1').text("USA GDP");
